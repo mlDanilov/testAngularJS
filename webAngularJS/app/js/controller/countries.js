80618518a378b8c0b0b75709e36c4e3aae763e2b
mlDanilov <den4iter@gmail.com>
2019-04-26 01:36:19 +1000

    first correct version with angularJS

--- webAngularJS/app/js/controller/countries.js
+++ webAngularJS/app/js/controller/countries.js
@@ -1,34 +1,37 @@
-﻿var cntrsApp = angular.module('countriesApp', []);
-cntrsApp.controller('countriesController', function ($scope) {
+﻿var countriesApp = angular.module('countriesApp', []);
+countriesApp.controller('countriesController', function ($scope) {
 
+$scope.Countries = [
+    {
+        Id : 1,
+        Name: "Россия",
+        Capital: "Москва",
+        Population: 146781095,
+        Area: 17125191
+    },
+    {
+        Id: 2,
+        Name: "Украина",
+        Capital: "Киев",
+        Population: 41953020,
+        Area: 603628
+    },
+    {
+        Id: 3,
+        Name: "Беларусь",
+        Capital: "Минск",
+        Population: 950380,
+        Area: 207595
+    },
+    {
+        Id: 4,
+        Name: "Польша",
+        Capital: "Варшава",
+        Population: 38422346,
+        Area:  312679
+    }
 
-    $scope.Countries = [
-        new {
-            Name: "Россия",
-            Capital: "Москва",
-            Population: 146781095,
-            Area: 17125191
-        },
-        new {
-            Name: "Украина",
-            Capital: "Киев",
-            Population: 41953020,
-            Area: 603628
-        },
-        new {
-            Name: "Беларусь",
-            Capital: "Минск",
-            Population: 950380,
-            Area: 207595
-        },
-        new {
-            Name: "Беларусь",
-            Capital: "Минск",
-            Population: 38422346,
-            Area:  312679
-        }
-
-    ];
+];
 
 
 });
